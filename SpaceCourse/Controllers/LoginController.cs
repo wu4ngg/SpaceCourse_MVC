81eaf92d9cdd841dae2e37fe661ff07470a696c8
fowardslash <68984861+fowardslash@users.noreply.github.com>
2022-11-16 12:53:25 +0700

    register :vvv

---
+++ SpaceCourse/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpaceCourse.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Main()
+        {
+            return View();
+        }
+    }
+}
